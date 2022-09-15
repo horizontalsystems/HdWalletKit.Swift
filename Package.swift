9d55edc22d5a4af6dbf78ef8e0ef8f4240126cb9
ant013 <ant013@mail.ru>
2022-09-15 15:31:17 +0400

    Downgrade secp256k1 version to 0.8.1, because of scalar_8x32.h not founded. (II)

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
-        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", exact: "0.8.1"),
+        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", "0.8.0"..."0.8.1"),
     ],
     targets: [
         .target(
